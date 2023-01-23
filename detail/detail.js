2b6466245fd443f79cd3a6e2c60a3c31a567fcc8
Jake Morgan <jakecmorgan.2000@gmail.com>
2023-01-23 15:28:28 -0800

    appended dogDetailEl to show dog info

--- detail/detail.js
+++ detail/detail.js
@@ -3,13 +3,11 @@ import { renderDogDetail } from '../render-utils.js';
 
 const dogDetailContainer = document.getElementById('dog-detail-container');
 
-// on load
 window.addEventListener('load', async () => {
-    // get the id from URL
     const data = new URLSearchParams(window.location.search);
-    // use the id to fetch the dog
     const id = data.get('id');
     const dog = await getDog(id);
-    // render and append this dog's details to the container
-    dogDetailContainer.append(JSON.stringify(dog));
+    const dogDetailEl = renderDogDetail(dog);
+
+    dogDetailContainer.append(dogDetailEl);
 });
